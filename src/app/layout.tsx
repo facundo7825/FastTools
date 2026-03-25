e346c14669ebf4550511beb50b994cf745875e3c
facundo <facundo7825@gmail.com>
2026-03-25 12:29:35 -0300

    chore: prepare layout for future ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,20 @@ export default function RootLayout({
     <html lang="es">
       <body className="min-h-screen flex flex-col">
         <Navbar />
+
+        {/* AD SLOT: top banner */}
+        <div id="ad-top" className="w-full max-w-5xl mx-auto px-4 pt-4" />
+
         <main className="flex-1 w-full max-w-5xl mx-auto px-4 py-8">
           {children}
+
+          {/* AD SLOT: in-content */}
+          <div id="ad-content" className="w-full mt-8" />
         </main>
+
+        {/* AD SLOT: above footer */}
+        <div id="ad-bottom" className="w-full max-w-5xl mx-auto px-4 pb-4" />
+
         <Footer />
       </body>
     </html>
